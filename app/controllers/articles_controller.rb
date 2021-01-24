7cf50d828a17b91f9998f9b6fe44a09761c550c5
niharika <niharika.singh@weinvest.net>
2021-01-24 16:13:31 +0530

    Article updated and edit

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -7,13 +7,36 @@ class ArticlesController < ApplicationController
     end   
 
     def new
+        @article=Article.new
     end
+    
+    def edit
+        
+        @article=Article.find(params[:id])
+
+    end
+    
     
     def create
         @article=Article.new(params.require(:article).permit(:title,:description))
-        @article.save
+        if @article.save
+          flash[:notice]  
         #redirect_to article_path(@article)
-        redirect_to@article
-    end     
+          redirect_to@article
+        else 
+            render 'new'  
+        end    
+    end   
+    
+    def update
+        @article=Article.find(params[:id])
+        if @article.update(params.require(:article).permit(:title,:description))
+            flash[:notice]="Article updated successfully"
+            redirect_to @article
+        else 
+            render 'edit'
+        end         
+
+    end    
 
 end   
